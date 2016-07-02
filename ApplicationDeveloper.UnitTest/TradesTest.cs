40b2a6639420c089cee3358b9bc5a9832e7c480b
kotaromak <kotaromak@gmail.com>
2016-07-01 19:53:08 -0700

    Added unit test project.

---
+++ ApplicationDeveloper.UnitTest/TradesTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using NUnit.Framework;
+using ApplicationDeveloper.Data.Repositories;
+using ApplicationDeveloper.Domain.Models;
+
+namespace ApplicationDeveloper.UnitTest
+{
+    [TestFixture]
+    public class TradesTest
+    {
+        private const int MaxCount = 500;
+
+        [Test]
+        public void GetTradesTest()
+        {
+            // Check to see if the trade count is correct.
+            var tradeRepository = new TradeRepository();
+            var trades = tradeRepository.GetTrades();
+            int tradeCount = 0;
+
+            foreach (var trade in trades)
+            {
+                tradeCount++;
+            }
+
+            Assert.AreEqual(MaxCount, tradeCount);
+        }
+    }
+}
